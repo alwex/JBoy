1e44f06b1c7928b9a9aa1a248566cda45d39d406
bob <bob@mail.com>
2011-12-15 14:00:13 +0100

    debugger v0.01

---
+++ src/main/java/com/alwex/jboy/utils/Debugger.java
@@ -0,0 +1,20 @@
+package com.alwex.jboy.utils;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author alex
+ */
+public class Debugger extends JFrame
+{
+
+    public Debugger()
+    {
+        setTitle("JBoy debugger");
+        setSize(300, 200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+}
